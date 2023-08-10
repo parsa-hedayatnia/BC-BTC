2b75bc7755f9da9678eb970038b8f1932b182ecb
parsa-hedayatnia <95084133+parsa-hedayatnia@users.noreply.github.com>
2023-08-10 12:48:29 +0330

    complete new block func and new trx func

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
+from datetime import time
+
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_trxs = []
 
-    def new_blocks(self):
+    def new_blocks(self,proof,previous_hash=None):
         #create a new block
-        pass
-    def new_trxs(self):
+        block = {
+            'index': len(self.chain)+1,
+            'timestamp' : time(),
+            'trxs' : self.current_trxs,
+            'proof' : proof,
+            'previous_hash' :previous_hash
+        }
+    def new_trxs(self,sender,recipient,amount):
         #add trx to the mempool
-        pass
+        self.current_trxs.append({'sender':sender ,'recipient': recipient , ' amount' : amount})
     @staticmethod
     def hash(block):
         #hashing algorithm
